74db790a04980e35a573b7dcb623c31dcc691787
pereskokovae <pereskokovae48@gmail.com>
2026-02-23 13:12:58 +0700

    add explosion animation on garbage hit

--- space_garbage.py
+++ space_garbage.py
@@ -1,5 +1,6 @@
 import asyncio
 from curses_tools import draw_frame, get_frame_size
+from expose import explode
 from obstacles import Obstacle
 
 obstacles = []
@@ -7,6 +8,8 @@ obstacles_in_last_collisions = []
 
 
 async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
+    global obstacles, obstacles_in_last_collisions
+
     rows_number, columns_number = canvas.getmaxyx()
     row = 0
 
@@ -14,13 +17,16 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
     column = max(0, min(column, columns_number - frame_columns - 1))
 
     obstacle = Obstacle(row, column, frame_rows, frame_columns)
-
     obstacles.append(obstacle)
 
     try:
         while row < rows_number:
             if obstacle in obstacles_in_last_collisions:
                 obstacles_in_last_collisions.remove(obstacle)
+
+                center_row = row + frame_rows / 2
+                center_column = column + frame_columns / 2
+                await explode(canvas, center_row, center_column)
                 return
 
             obstacle.row = row
@@ -28,7 +34,6 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
 
             draw_frame(canvas, row, column, garbage_frame)
             await asyncio.sleep(0)
-
             draw_frame(canvas, row, column, garbage_frame, negative=True)
 
             row += speed
@@ -36,6 +41,3 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
     finally:
         if obstacle in obstacles:
             obstacles.remove(obstacle)
-
-        if obstacle in obstacles_in_last_collisions:
-            obstacles_in_last_collisions.remove(obstacle)
